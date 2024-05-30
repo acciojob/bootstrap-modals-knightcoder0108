ecc15adefb4d801bab10f9f2f2c8d13f79adc7f1
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-05-31 02:22:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+it("should display the modal when the button is clicked", () => {
+    cy.visit(baseUrl + "/main.html");
+    cy.get("#newton-modal").should("exist");
+    cy.get('#open-modal').click();
+    cy.get("#newton-modal").should('be.visible');
+});
+it("should hide the modal when the close button is clicked", () => {
+    cy.visit(baseUrl + "/main.html");
+    cy.get('#open-modal').click();
+    cy.get("#newton-modal").should('be.visible');
+    cy.get('.modal-footer button').click();
+    cy.get("#newton-modal").should('not.be.visible');
+});
